fab01c21ceca6d6f298ae669ed81d13acee2fa02
Aidah Al Baqir <aidah.albaqir@gmail.com>
2021-07-26 19:41:53 -0500

    commit tree b30c913e60f29d4def452f2fc423bb830e36da3a

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ function addingEventListener() {
         alert('I was clicked!');
       }
       
-      input.addEventListener('click', clickAlert); 
+      input.addEventListener('click', clickAlert);       
 }
+
+// let input = document.getElementById ('input')
+// function addingEventListener() {
+//   function clickAlert() {
+//       alert('I was clicked!');
+//   }
+// }
